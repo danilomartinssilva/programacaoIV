4ca1e059c73b43995e1ccef47f0dfe2f54e612a0
Danilo Martins <danilomartinssilva@r7.com>
2018-08-29 15:57:45 -0300

    Pronto pra aula

--- server/server.ts
+++ server/server.ts
@@ -9,7 +9,7 @@ export class Server {
 
   application: restify.Server
 
-  initRoutes(): Promise<any>{
+  initRoutes(routers:Router[]): Promise<any>{
     return new Promise((resolve, reject)=>{
       try{
 
@@ -20,34 +20,11 @@ export class Server {
 
         this.application.use(restify.plugins.queryParser())
 
-        //routes
+        for(let router of routers ){
+            router.applyRoutes(this.application);
+        }
 
-        this.application.get('/info', [
-          (req, resp, next)=>{
-            if(req.userAgent() && req.userAgent().includes('MSIE 7.0')){
-            //resp.status(400)
-          //  resp.json({message: 'Please, update your browser'})
-            let error: any = new Error()
-            error.statusCode = 400
-            error.message = 'Please, update your browser'
-            return next(error)
-          }
-          return next()
-        },(req, resp, next)=>{
-          //resp.contentType = 'application/json';
-          //resp.status(400)
-          //resp.setHeader('Content-Type','application/json')
-          //resp.send({message: 'hello'});
-          resp.json({
-            browser: req.userAgent(),
-            method: req.method,
-            url: req.href(),
-            path: req.path(),
-            query: req.query
-          })
-          return next()
-        }])
-
+     
         this.application.listen(environment.server.port, ()=>{
            resolve(this.application)
         })
@@ -58,8 +35,8 @@ export class Server {
     })
   }
 
-  bootstrap(): Promise<Server>{
-      return this.initRoutes().then(()=> this)
+  bootstrap(routers:Router[]=[]): Promise<Server>{
+      return this.initRoutes(routers).then(()=> this)
   }
 
 }
